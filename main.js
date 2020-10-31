96d25fbe776721cecd30380043237ed5bd6bc7c1
PedroMota341 <73261125+PedroMota341@users.noreply.github.com>
2020-10-31 00:09:16 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var random = Math.floor(Math.random()*10)+1; //Criando um sistema de random de números de 1 a 10
+
+if(random == 5) { //Se o random cair no número 5, ele vai dar:
+console.log('CORRETO!');
+}else { //Se não cair no número 5, ele vai dar:
+console.log('ERRADO!');
+}
